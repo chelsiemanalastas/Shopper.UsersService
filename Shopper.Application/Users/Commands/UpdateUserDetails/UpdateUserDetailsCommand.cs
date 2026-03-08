46eba0ed056f7f8cb08d1b9b5f473887b3bad8e8
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-03-08 22:38:15 +0800

    Develop update user command, handler, and controller
    - Register MediatR in application layer dependency injection

---
+++ Shopper.Application/Users/Commands/UpdateUserDetails/UpdateUserDetailsCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Shopper.Application.Users.Commands.UpdateUserDetails;
+
+public record UpdateUserDetailsCommand : IRequest
+{
+    public DateOnly? DateOfBirth { get; set; }
+    public string? Country { get; set; }
+}
