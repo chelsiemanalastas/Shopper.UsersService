8b4438b416c3a461c759c4fe11c67ed57e35f2c2
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-03-08 14:09:02 +0800

    Initial migration for users db setup and usersdbcontext persistence

--- Shopper.Infrastructure/DependencyInjection.cs
+++ Shopper.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shopper.Infrastructure.Persistence;
 
 namespace Shopper.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
     {
+        var connectionString = config.GetConnectionString("UsersDb");
+        services.AddDbContext<UsersDbContext>(options =>
+            options.UseSqlServer(connectionString).EnableSensitiveDataLogging()
+        );
+
         return services;
     }
 }
---
+++ Shopper.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Shopper.Infrastructure.Identity;
+
+// Extending Entity Framework IdentityUser properties
+public class ApplicationUser : IdentityUser
+{
+    public DateOnly? DateOfBirth { get; set; }
+    public string? Country { get; set; }
+}
